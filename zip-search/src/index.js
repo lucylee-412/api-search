4c8c4c783a13bbbe659a6b9e46eb9a28254fcb59
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 16:12:57 -0500

    Create Search component and isZipCode() validator
    Search component handles user input for zip code field.
    isZipCode compares against a regex to check if user input is a valid
    5-digit ZIP code.

--- zip-search/src/index.js
+++ zip-search/src/index.js
@@ -1,3 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// Zip code validation check
+function isZipCode(str)
+{
+    return /^\d{5}(-\d{4})?$/.test(str);
+}
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        if (!isZipCode(this.state.value)) {
+            alert(this.state.value + ' is not a zip code.');
+        }
+
+        alert('A zip code was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Zip Code:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+);
